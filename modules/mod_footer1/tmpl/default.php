708935ba627f70fbc3e12bd5af63bef096f25045
Khỉ Con <khicononline9xtq@icloud.com>
2015-07-24 17:28:36 +0700

    SYnc

--- modules/mod_footer1/tmpl/default.php
+++ modules/mod_footer1/tmpl/default.php
@@ -8,14 +8,16 @@
 defined('_JEXEC') or die('Restricted access');
 ?>  <div class="wpb_wrapper">
                     <div class="vc_custom_heading vc_custom_1432700813827" >
-                    <h3 style="font-size: 25px;color: #3290dc;line-height: 28px;text-align: center;font-family:Raleway;font-weight:700;font-style:normal" >CLEAN AND UNIQUE PSD FOR YOUR WEBSITE!</h3></div>
+                    <h3 style="font-size: 25px;color: #3290dc;line-height: 28px;text-align: center;font-family:Raleway;font-weight:700;font-style:normal" >
+                    Hãy liên hệ với chung tôi để được tư vấn hỗ trợ!</h3></div>
             <div class="wpb_text_column wpb_content_element ">
                 <div class="wpb_wrapper">
-                    <p>Quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores ratione voluptatem<br />
-        Neque porro quisquam est, qui dolorem ipsum quia dolor amet.</p>
+                    <p>Bạn cần trợ giúp về sản phẩm của chúng tôi.
+                     Hãy lòng liên hệ với chúng tôi để chúng tôi có cơ  hội phục vụ bạn !.</p>
 
                 </div>
-            </div> <a href='#' target='_self' class='btn  '>Buy Now</a><a href='#' target='_self' class='btn  '>Contact Us</a>  <div class="vc_empty_space"  style="height: 0px" ><span
+            </div> <a href='#' target='_self' class='btn  '>Xem sản phẩm</a>
+            <a href='#' target='_self' class='btn  '>Liên hệ</a>  <div class="vc_empty_space"  style="height: 0px" ><span
                     class="vc_empty_space_inner"></span></div>
 
 
--- modules/mod_footer2/tmpl/default.php
+++ modules/mod_footer2/tmpl/default.php
@@ -12,43 +12,65 @@ defined('_JEXEC') or die('Restricted access');
                         <!-- Footer heading
                 ================================================== -->
                 <div class="footer-heading">
-                    <h5>HAVE ANY QUESTIONS OR WANT A FREE ESTIMATE?</h5>
-                    <h3>CALL NOW: +(84) 1627 58 3467</h3>
+                    <h5>Bạn có thắc mắc.Hãy gọi cho chúng tôi?</h5>
+                    <h3>Miễn phí : (84) 06-46.256.352 </h3>
                 </div>
 
-                <div class='footer-column-1 col-md-3 col-sm-6'><aside id="categories-3" class="widget widget_categories"><h3>Categories</h3>        <ul>
-            <li class="cat-item cat-item-1"><a href="category/service/index.html" >Complete Cleaning Services</a>
+                <div class='footer-column-1 col-md-3 col-sm-6'><aside id="categories-3" class="widget widget_categories"><h3>
+                Danh mục</h3>        <ul>
+            <li class="cat-item cat-item-1"><a href="category/service/index.html" >
+            Chăm sóc khách hàng</a>
         </li>
-            <li class="cat-item cat-item-7"><a href="category/home-posts/index.html" >Home posts</a>
+            <li class="cat-item cat-item-7"><a href="category/home-posts/index.html" >Tin tức</a>
         </li>
-            <li class="cat-item cat-item-2"><a href="category/cleaning/index.html" >House Cleaning</a>
+            <li class="cat-item cat-item-2"><a href="category/cleaning/index.html" >Sản phẩm</a>
         </li>
                 </ul>
-        </aside></div><div class='footer-column-2 col-md-3 col-sm-6'><aside id="tt_addresswidget-2" class="widget widget_address"><h3>Contact Info</h3><address><abbr class="address" title="address">Architectura Worldwide Foundation 125/68 North 38th Str, Melbourne, VIC.</abbr><abbr title="phone">(305) 533-1122</abbr><abbr title="email"><a href="mailto:info@architectura.com">info@architectura.com</a></abbr><abbr title="web"><a href="http://architecturahqs.com/">http://architecturahqs.com</a></abbr></address></aside></div><div class='footer-column-3 col-md-3 col-sm-6'><aside id="tt_recentpostswidget-2" class="widget widget_recent_post"><div class='recent_post'><div class="col-md-8 col-sm-6 col-xs-7">
-                            <a href="2015/05/30/12-tips-to-clean-your-office-window-clearly/index.html"><h5>12 tips to clean your office window clearly</h5></a>
-                            <div class="date">May 30, 2015</div>
+        </aside></div><div class='footer-column-2 col-md-3 col-sm-6'>
+        <aside id="tt_addresswidget-2" class="widget widget_address">
+        <h3>Thông tin liên hệ</h3><address>
+        <abbr class="address" title="address">Tại Vũng Tàu – Việt Nam:
+Phan Kế Bính ST.8, Q. 9, Vũng Tàu, Việt Nam
+Thời gian làm
+văn phòng:
+M-F 8:00 đến 6 giờ chiều PT (GMT +7)
+</abbr>
+        <abbr title="phone">(84) 06-46.256.352 </abbr>
+        <abbr title="email"><a href="mailto:sales@sdvico.com ">sales@sdvico.com </a></abbr>
+        <abbr title="web"><a href="http://sdvico.com/">http://sdvico.com/</a></abbr>
+        </address></aside></div><div class='footer-column-3 col-md-3 col-sm-6'>
+        <aside id="tt_recentpostswidget-2" class="widget widget_recent_post">
+        <div class='recent_post'><div class="col-md-8 col-sm-6 col-xs-7">
+                            <a href="2015/05/30/12-tips-to-clean-your-office-window-clearly/index.html">
+                            <h5>Các mẹo khi sử dụng sản phẩm</h5></a>
+                            <div class="date">30/5/2015</div>
                         </div><div class="col-md-4 col-sm-5 col-xs-5">
                                       <a href="2015/05/30/12-tips-to-clean-your-office-window-clearly/index.html"><img width="92" height="72" src="<?php echo JURI::base();?>templates/sdvico/wp-content/uploads/sites/29/2015/05/blog-list-1-92x72.jpg" class="attachment-recent-posts-thumb wp-post-image" alt="Service" /></a>
                                   </div></div><div class='recent_post'><div class="col-md-8 col-sm-6 col-xs-7">
-                            <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html"><h5>Next week we increase green cleaning</h5></a>
-                            <div class="date">May 30, 2015</div>
+                            <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html"><h5>
+                            Những lợi ích của sản phẩm</h5></a>
+                            <div class="date">30/5/2015</div>
                         </div><div class="col-md-4 col-sm-5 col-xs-5">
-                                      <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html"><img width="92" height="72" src="<?php echo JURI::base();?>templates/sdvico/wp-content/uploads/sites/29/2015/05/blog-list-3-92x72.jpg" class="attachment-recent-posts-thumb wp-post-image" alt="Service" /></a>
-                                  </div></div></aside></div><div class='footer-column-4 col-md-3 col-sm-6'>     <aside id="recent-posts-3" class="widget widget_recent_entries">        <h3>Recent Posts</h3>       <ul>
+                                      <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html">
+                                      <img width="92" height="72" src="<?php echo JURI::base();?>templates/sdvico/wp-content/uploads/sites/29/2015/05/blog-list-3-92x72.jpg" class="attachment-recent-posts-thumb wp-post-image" alt="Service" /></a>
+                                  </div></div></aside></div><div class='footer-column-4 col-md-3 col-sm-6'>     <aside id="recent-posts-3" class="widget widget_recent_entries">        <h3>Bài gần đây</h3>       <ul>
                             <li>
-                        <a href="2015/05/30/12-tips-to-clean-your-office-window-clearly/index.html">12 tips to clean your office window clearly</a>
+                        <a href="2015/05/30/12-tips-to-clean-your-office-window-clearly/index.html">
+                        Các mẹo khi sử dụng sản phẩm</a>
                                 </li>
                             <li>
-                        <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html">Next week we increase green cleaning</a>
+                        <a href="2015/05/30/next-week-we-increase-green-cleaning/index.html">
+                        Lợi ích sử dụng sản phẩm</a>
                                 </li>
                             <li>
-                        <a href="2015/04/30/we-are-ready-to-clean-your-world/index.html">We are ready to clean your world</a>
+                        <a href="2015/04/30/we-are-ready-to-clean-your-world/index.html">
+                        Tài trợ máy lọc nước cho đảo trường sa</a>
                                 </li>
                             <li>
-                        <a href="2015/04/30/hello-world/index.html">12 tips to clean window efficiently</a>
+                        <a href="2015/04/30/hello-world/index.html">Tài trợ máy lọc nước cho hộ nghèo</a>
                                 </li>
                             <li>
-                        <a href="2015/03/30/qulaity-cleaning-tools/index.html">Qulaity cleaning tools</a>
+                        <a href="2015/03/30/qulaity-cleaning-tools/index.html">Sản phẩm bán chạy</a>
                                 </li>
                         </ul>
                 </aside></div>
@@ -59,7 +81,8 @@ defined('_JEXEC') or die('Restricted access');
 
 
                             <div class="col-sm-12">
-                                <div id="text-2" class="widget widget_text">            <div class="textwidget">Copyright © 2015 Make Clean. All Rights Reserved.</div>
+                                <div id="text-2" class="widget widget_text">
+                                <div class="textwidget">Copyright © 2015 SDVICO BY  <a href="http://rdcma.com"> RDCMA.COM</a>.   All Rights Reserved.</div>
                 </div><div id="tt_sociallinkswidget-2" class="widget widget_social"><ul class="social-icon list-inline"><li><a class="social-facebook fa fa-facebook tooltipped" href="#!" data-position="bottom" data-delay="30" data-tooltip="facebook"></a></li><li><a class="social-twitter fa fa-twitter tooltipped" href="#!" data-position="bottom" data-delay="30" data-tooltip="twitter"></a></li><li><a class="social-pinterest fa fa-pinterest tooltipped" href="#!" data-position="bottom" data-delay="30" data-tooltip="pinterest"></a></li><li><a class="social-instagram fa fa-instagram tooltipped" href="#!" data-position="bottom" data-delay="30" data-tooltip="instagram"></a></li></ul></div>                    </div>
 
 
