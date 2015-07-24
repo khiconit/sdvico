0a3dbb701a65ac212027b0796cb52c3531aa4bfa
Khỉ Con <khicononline9xtq@icloud.com>
2015-07-24 16:42:33 +0700

    Commit

--- modules/mod_jvlatest_news/tmpl/default/default.php
+++ modules/mod_jvlatest_news/tmpl/default/default.php
@@ -10,7 +10,7 @@
  # Technical Support:  http://www.joomlavi.com/my-tickets.html
 -------------------------------------------------------------------------*/
 
-// No direct access to this file 
+// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
 $document = JFactory::getDocument();
@@ -19,18 +19,18 @@ $document->addStyleSheet(JURI::base().'modules/mod_jvlatest_news/tmpl/'.$jvtmpl.
 JHTML::addIncludePath(JPATH_SITE.'/components/com_content/helpers');
 JHTML::_('behavior.framework', true);
 if(count($items)){
-    
+
     if(!JRequest::getVar('jvlatestnews-ajax')){ ?>
     <div id="jvlatestnews<?php echo $module->id ?>" class="jvlatestnews <?php echo $jvtmpl ;?>">
-    
+
     	<?php if ( $params->get('description') != '' ) { ?>
     	<div class="description"><?php echo $params->get('description'); ?></div>
     <?php } ?>
-    
-        <div class="jvlatestnews-container<?php echo $jvtmpl != '' ?  ' '.$jvtmpl : ''; ?><?php echo $moduleclass_sfx != '' ?  ' '.$moduleclass_sfx : ''; ?>">    
+
+        <div class="jvlatestnews-container<?php echo $jvtmpl != '' ?  ' '.$jvtmpl : ''; ?><?php echo $moduleclass_sfx != '' ?  ' '.$moduleclass_sfx : ''; ?>">
             <div class="jvlatestnews-content">
             <?php } ?>
-            <?php 
+            <?php
                 if($params->get('group_cat')) {
                     foreach ($items as $catTitle=>$list){
                         require JModuleHelper::getLayoutPath('mod_jvlatest_news', $jvtmpl.'/article_lists');
@@ -40,26 +40,26 @@ if(count($items)){
                     require JModuleHelper::getLayoutPath('mod_jvlatest_news', $jvtmpl.'/article_lists');
                 }
             ?>
-            <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>     
+            <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>
             </div>
             <?php } ?>
 
         <?php  if($pagination->get('pages.total') && $params->get('show_paging', 0)){
             for($i = 1; $i < $pagination->get('pages.total'); $i++){
-                if(!JRequest::getVar('jvlatestnews-ajax')){  ?>     
-                <div class="jvlatestnews-content"></div> 
-                <?php } 
-            } 
+                if(!JRequest::getVar('jvlatestnews-ajax')){  ?>
+                <div class="jvlatestnews-content"></div>
+                <?php }
+            }
         } ?>
 
-        <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>     
+        <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>
         </div>
         <?php } ?>
-    <?php if(JRequest::getVar('jvlatestnews-ajax') && JRequest::getVar('mid') == $module->id){ die;}  ?> 
+    <?php if(JRequest::getVar('jvlatestnews-ajax') && JRequest::getVar('mid') == $module->id){ die;}  ?>
     <?php if($params->get('show_paging', 0) && count($pagination)){
-        require JModuleHelper::getLayoutPath('mod_jvlatest_news', $jvtmpl.'/article_pagination');        
-    } ?> 
-    <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>     
+        require JModuleHelper::getLayoutPath('mod_jvlatest_news', $jvtmpl.'/article_pagination');
+    } ?>
+    <?php if(!JRequest::getVar('jvlatestnews-ajax')){  ?>
     </div>
     <?php } ?>
 
