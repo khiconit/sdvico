ed2124db236b600327e7dee21cb81bb0d4d688f5
chiendaotran <chiencom10@gmail.com>
2015-07-25 11:39:09 +0700

    ban do

--- modules/mod_googlemaps/tmpl/default.php
+++ modules/mod_googlemaps/tmpl/default.php
@@ -7,4 +7,4 @@
 
 defined('_JEXEC') or die('Restricted access');
 ?>
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22200.83674933232!2d107.10155048296595!3d10.374586141569589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1437793230481" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22200.83674933232!2d107.10155048296595!3d10.374586141569589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1437793230481" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
